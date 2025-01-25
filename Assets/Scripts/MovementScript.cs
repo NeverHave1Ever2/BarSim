21e18ec72173cac5936fe9ce5963b02fb43b7c90
Noctwoa <edwardpaullynch@gmail.com>
2025-01-25 14:30:53 +0000

    Movement Script
    no velocity clamper

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovementScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public Vector3 moveDirection;
+    public float horizontalInput;
+    public float verticalInput;
+    public Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    void MovePlayer()
+    {
+        moveDirection = new Vector3(horizontalInput, 0f, verticalInput);
+        rb.AddForce(moveDirection * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+}
