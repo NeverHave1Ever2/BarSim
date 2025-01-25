1b7e85b2ce706f6fb4b45b67c290aa94cb16c0d7
NeverHave1Ever <117602567+NeverHave1Ever@users.noreply.github.com>
2025-01-25 14:11:43 +0000

    MinigameOpen

--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -3,16 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class MiniGameManager : MonoBehaviour
 {
+    bool MGPlaying = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
-        SceneManager.LoadScene("MGScene", LoadSceneMode.Additive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return) && !MGPlaying)
+        {
+            MGPlaying = true;
+            SceneManager.LoadScene("MGScene", LoadSceneMode.Additive);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && MGPlaying)
+        {
+            MGPlaying = false;
+            SceneManager.UnloadSceneAsync("MGScene");
+        }
+
     }
 }
