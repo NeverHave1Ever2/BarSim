2d40f118b38eb7376650aa53dafa6d8d3c2c342a
ethrealm <etherealmalls@gmail.com>
2025-01-26 15:48:46 +0000

    might have to kill myself after this one!

--- Assets/Scripts/BeerMinigameInteraction.cs
+++ Assets/Scripts/BeerMinigameInteraction.cs
@@ -6,41 +6,39 @@ public class BeerMinigameInteraction : MonoBehaviour
 {
     [SerializeField] public GameObject progressPanel;
     [SerializeField] public Slider progressBar;
-    public bool held;
-    public Image beerGlass;
+    public bool held = false;
     public Sprite fullBeerGlassImage;
     public SpriteRenderer sr;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
         fullBeerGlassImage = GetComponent<Sprite>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             held = true;
 
         } else if (Input.GetKeyUp(KeyCode.Space)){
             held = false;
         }
+        if (held == false & progressBar.value != 0)
+        {
+            Destroy(progressPanel);
+            if (progressBar.value !< 700 ^ progressBar.value !> 800)
+            {
+                sr.sprite = fullBeerGlassImage;
+            }
+        }
         if (held)
         {
             progressBar.value += 1;
         }
-        if (held = false & progressBar.value != 0)
-        {
-            if (progressBar.value < 700 || progressBar.value > 800)
-            {
-                Destroy(progressBar);
-                beerGlass.sprite = 
-            }
-        }
-        
 
     }
     
